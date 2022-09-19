0b0982f1fb1758227bb1838caf7b8b3e52e1137d
Lanre Akintayo <akintayolanre2019@gmail.com>
2022-09-19 12:05:07 +0100

    Updated

--- contracts/Dao.sol
+++ contracts/Dao.sol
@@ -30,8 +30,9 @@ contract Dao is ReentrancyGuard {
     }
 
     struct Option {
-        uint index;
+        uint256 index;
         string optionText;
+        uint vote;
     }
 
     struct Proposal {
@@ -41,11 +42,29 @@ contract Dao is ReentrancyGuard {
         string description;
         ProposalType proposalType;
         ProposalStatus proposalStatus;
-        uint startDate;
-        uint duration;
+        uint256 startDate;
+        uint256 duration;
         Option[] options;
-        
+        address[] voters;
     }
+
+    event ProposalCreated(
+        uint256 id,
+        address creator,
+        string title,
+        string description,
+        ProposalType proposalType,
+        ProposalStatus proposalStatus,
+        uint256 startDate,
+        uint256 duration,
+        Option[] options
+    );
+
+    event QuadraticVote(
+        uint id,
+        address voter,
+        Option[] options
+    )
 
     mapping(uint256 => Proposal) public proposals;
 
@@ -58,8 +77,8 @@ contract Dao is ReentrancyGuard {
         string memory _description,
         uint256 _proposalType,
         uint256 _proposalStatus,
-        uint _startDate,
-        uint _duration,
+        uint256 _startDate,
+        uint256 _duration,
         Option[] memory _options
     ) external nonReentrant {
         require(
@@ -103,6 +122,80 @@ contract Dao is ReentrancyGuard {
 
         proposals[proposalId] = proposal;
 
-        emit ProposalCreated()
+        emit ProposalCreated(
+            proposalId,
+            msg.sender,
+            _title,
+            _description,
+            proposalType,
+            proposalStatus,
+            _startDate,
+            _duration,
+            options
+        );
     }
+
+    function voteProposalByQuadratic(
+        uint256 id,
+        uint256[] memory indexes,
+        uint256[] memory votingPower
+    ) external nonReentrant {
+        uint totalVotingPower = getQuadraticTotalVotingPower(votingPower);
+        int hasVoted = checkVotingStatus(id, msg.sender)
+        Proposal memory proposal = proposals[id];
+
+        require(block.timestamp < proposal.startDate + duration, "Proposal has closed");
+        require(hasVoted < 0, "You've voted already");
+        require(larToken.balanceOf(msg.sender) >= totalVotingPower, "Insufficient Voting Power");
+        larToken.transferFrom(msg.sender, address(this), totalVotingPower);
+
+        Option[] storage options = proposals[id].options;
+
+        for (uint i = 0; i < indexes.length; i++){
+            uint currentOptionIndex = indexes[i];
+            uint currentOptionVotingPower = indexes[i];
+            options[currentOptionIndex].vote += sqrt(currentOptionVotingPower);
+        }
+
+        proposals[id].voters.push(msg.sender)
+
+        emit QuadraticVote(id, msg.sender, options)
+    }
+
+    function voteProposalBySingleChoice(uint id, uint index, uint votingPower) external nonReentrant {
+        require(larToken.balanceOf(msg.sender) >= votingPower, "Insufficient voting Power");
+        larToken.transferFrom(msg.sender, address(this), votingPower);
+        proposals[id].options[index].vote += votingPower;
+
+    } 
+
+    function getQuadraticTotalVotingPower(uint256[] memory votingPower) internal returns(uint256 totalVotingPower) {
+        for (uint i = 0; i < votingPower.length; i++){
+            totalVotingPower += votingPower[i];
+        }
+    }
+
+    function checkVotingStatus(uint id, address voter) internal view returns(int){
+        address[] memory voters = proposals[id].voters;
+        for (uint i = 0; i < voters.length; i++){
+            address currentVoter = voters[i];
+            if (voter == currentVoter){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    function sqrt(uint y) public pure returns (uint z) {
+    if (y > 3) {
+        z = y;
+        uint x = y / 2 + 1;
+        while (x < z) {
+            z = x;
+            x = (y / x + x) / 2;
+        }
+    } else if (y != 0) {
+        z = 1;
+    }
+}
 }
